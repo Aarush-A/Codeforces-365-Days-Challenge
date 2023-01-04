3ab2486df3ae53a99089a6fa9b3a9ca399d312a7
Aarush Agarwal <aarusha870@gmail.com>
2023-01-04 14:23:06 +0530

    4th January

---
+++ 1.4_282A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n,x=0;
+    string str;
+    cin>>n;
+    while(n--){
+        cin>>str;
+        if(str[1]=='+'){
+            ++x;
+        }
+        else{
+            --x;
+        }
+    }
+    cout<<x;
+}
